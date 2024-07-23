651f31bb3263d657234582116e4d3e702e7acf0f
omar karake <omar.karake@amalitechtraining.org>
2024-07-23 16:43:35 +0200

    bubble sort inhanced

--- index.js
+++ index.js
@@ -326,16 +326,20 @@ function bubbleSort(arr) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
+  let noSwap;
   for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
     for (let j = 0; j < i - 1; j++) {
-      console.log(arr, arr[j], arr[j + 1]);
+      console.log(arr, arr[j], arr[j + 1], noSwap);
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
+      noSwap = false;
     }
+    if (noSwap) break;
     console.log("one pass complete!");
   }
   return arr;
 }
 
-console.log("returned: ", bubbleSort([3, 5, 6, 72, -5, 1, 2]));
+console.log("returned: ", bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
