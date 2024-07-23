4fe4eb410c489227fe9cfeb2047cadda1ae6ad15
omar karake <omar.karake@amalitechtraining.org>
2024-07-23 19:26:20 +0200

    insertion sort

--- index.js
+++ index.js
@@ -346,21 +346,37 @@ GOOD LUCK �
 
 // ------------------------------------------------------------------
 // selection sort
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let lowest = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[lowest] > arr[j]) {
-        lowest = j;
-      }
+// function selectionSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let lowest = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[lowest] > arr[j]) {
+//         lowest = j;
+//       }
+//     }
+//     if (i !== lowest) {
+//       let temp = arr[i];
+//       arr[i] = arr[lowest];
+//       arr[lowest] = temp;
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(selectionSort([5, 3, 4, 1, 2]));
+// ------------------------------------------------
+// insertion sort
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
     }
-    if (i !== lowest) {
-      let temp = arr[i];
-      arr[i] = arr[lowest];
-      arr[lowest] = temp;
-    }
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
 
-console.log(selectionSort([5, 3, 4, 1, 2]));
+console.log(insertionSort([5, 3, 4, 1, 2]));
+
